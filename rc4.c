28a399c1b51ab5097b6a3e7938b065f6464ee281
Mai Huijun <m.maihuijun@gmail.com>
2011-10-19 18:45:33 +0800

    Add two files:rc4.h,rc4.c. The algorithm of RC4.

--- rc4.c
+++ rc4.c
@@ -1,35 +1,49 @@
-#include <stdio.h>
+#include <string.h>
+#include "rc4.h"
 
-int main(){
-    int i, j, temp, keylength = 3;
-    unsigned char S[256], K[3] = "abc";
-    
-    /*S is initialized to the identity permutation, mixes in bytes of the key.*/
+/*S is initialized to the identity permutation, mixes in bytes of the key.*/
+void RC4_key(RC4_KEY* rc4_key, unsigned char* key, int keylength){
+    int i, j, temp;
+    unsigned char K[256];
+
+    /*Initialize S*/
     for (i = 0; i < 256; i++)
-	S[i] = i;
+	rc4_key -> S[i] = i;
+
+    /*Initialize K*/
+    j = 0;
+    for (i = 0; i < 256; i++){
+	K[i] = key[j % keylength];
+	j++;
+    }
 
     j = 0;
     for (i = 0; i < 256; i++){
-	j = (j + S[i] + K[i % keylength]) % 256;
-	/*Swap S[i] and S[j]*/
-	temp = S[i];
-	S[i] = S[j];
-	S[j] = temp;
+	j = (j + rc4_key -> S[i] + K[i]) % 256;
+	/*Swap rc4_key -> S[i] and S[j]*/
+	temp = rc4_key -> S[i];
+	rc4_key -> S[i] = rc4 -> S[j];
+	rc4_key -> S[j] = temp;
     }
+}
+
+/*Encrypt the plaintext and output the ciphertext.*/
+void RC4(RC4_KEY* rc4_key, unsigned char* plaintext, int plaintext_length, unsigned char* ciphertext){
+    int i, j, n;
     
-    /*Output the keystream by pseudo-random number generator.*/
     i = 0;
     j = 0;
-    while(1){
+    for (i = 0; i < plaintext_length; i++){
 	i = (i + 1) % 256;
-	j = (j + S[i]) % 256;
-	/*Swap S[i] and S[j]*/
-	temp = S[i];
-	S[i] = S[j];
-	S[j] = temp;
-	/*generate K*/
-	K[0] = S[(S[i] + S[j]) % 256];
-	printf("%s\n", K[0]);
+	j = (j + rc4_key -> S[i]) % 256;
+	/*Swap rc4_key -> S[i] and rc4_key -> S[j]*/
+	temp = rc4_key -> S[i];
+	rc4_key -> S[i] = rc4_key -> S[j];
+	rc4_key -> S[j] = temp;
+
+	n = rc4_key -> S[(rc4_key -> S[i] + rc4_key -> S[j]) % 256];
+	
+	/*Encryption*/
+	ciphertext[i] = plaintext[i] ^ n;
     }
-    return 0;
 }
---
+++ rc4.h
@@ -0,0 +1,14 @@
+#ifndef RC4_H
+#define RC4_H
+
+typedef struct RC4_KEY_S{
+    unsigned char S[256];
+}RC4_KEY;
+
+/*S is initialized to the identity permutation, mixes in bytes of the key.*/
+void RC4_key(RC4_KEY* rc4_key, unsigned char* key, int keylength);
+
+/*Encrypt the plaintext and output the ciphertext.*/
+void RC4(RC4_KEY* rc4_key, unsigned char* plaintext, int plaintext_length, unsigned char* ciphertext);
+
+#endif
